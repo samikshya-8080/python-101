2db143c2ff249afbd84fabe8b0f9033a6f759eee
samikshya <132491565+samikshya-8080@users.noreply.github.com>
2023-05-06 17:56:43 +0545

    Create variables.py
    -learned about data types and comments.
    -types of comment
    -types of data types
    -variable declaration with examples.

---
+++ variables.py
@@ -0,0 +1,78 @@
+### Two Types of comments 
+# 1. Single line comment
+#yo vanya chai l line ma matra kam garxa 
+# 2. Multi line comment
+"""
+example of multiline comment
+more comments
+"""
+
+
+# Data Types 
+# We have different types of data types in python
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# None Type:	NoneType
+# Boolean Type:	bool
+
+
+# ----- learn it later with other concepts ---------
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Binary Types:	bytes, bytearray, memoryview
+
+
+#1.  Numberic Types:	int, float, complex
+    
+    # int 
+        # int is interger so you might have read it from maths to all the negative to postive numbers are the interger
+        # e.x. -1,0,1,2,3...... -2,-3,-4
+        
+    # float
+        # float is basically decimal so it only holds decimal values
+        # ex.  -1.0,1.0,0.0 so on
+
+    # complex numbers 
+        # the same thing as math so if a number has real and imaginery part with it it is complex
+        # ex. 1j 3i 
+    
+# 2. Boolean Types: bool
+    # bool mean True or False.
+    # ex. True, False
+    #  are you above 18? -> either True or False
+    
+# 3. Text Types: str
+    # str is basically string so it can hold any kind of data. it should be either inside ' here anything is considerd as text' or "1234 it is text "
+    # ex. "hello", "world"
+
+# 4. None Type: None
+    # None is a special type of data type that is used to represent null values. (null,empty,Nothing).
+    # data nai hold gardaina kei xaina vane None lekhne bujhyou?
+
+
+#  Now comes Variable declarations
+
+
+# name:cast = his values  yesari variable banainxa
+
+# some rules before
+    # 1. variable name ma bich ma space huna hunna if space chianxa vane 
+        # use underscore or camelCase
+        # e.g my_name_is or myNameIs 
+    # 2. variable ko data type
+        # decimal_number: int = 2.0 (this is wrong)
+        # decimal_number: float = 2.0 (this is correct)
+    
+# integer -> int
+# string -> str
+# decimal -> float
+# boolean -> bool
+
+first_name:str = "Name"
+middle_name:str = None
+last_name:str = "Name"
+email:str = "name@gmail.com"
+permanent_address:str = ""
+date_of_birth:str = ""
